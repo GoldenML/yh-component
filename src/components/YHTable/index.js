f39a0afe7b32c0dbfcd84b14fcc74f217303f776
GoldenML <wayy180304@gmail.com>
2021-02-08 14:31:52 +0800

    修改

---
+++ src/components/YHTable/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import { Table } from 'antd'
+
+class index extends PureComponent {
+  render() {
+    return (
+      <Table />
+    )
+  }
+}
+export default index
+
